6f5afd0242f9ccab3e58c3fa726deba7bae8b13b
Likhithalikki123 <dummy@gmail.com>
2026-03-14 04:17:07 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/dao/CustomerDAO.java
+++ server/src/main/java/com/edutech/progressive/dao/CustomerDAO.java
@@ -1,11 +1,10 @@
 package com.edutech.progressive.dao;
 
+import com.edutech.progressive.dto.CustomerAccountInfo;
+import com.edutech.progressive.entity.Customers;
 
 import java.sql.SQLException;
 import java.util.List;
-
-import com.edutech.progressive.dto.CustomerAccountInfo;
-import com.edutech.progressive.entity.Customers;
 
 public interface CustomerDAO {
     int addCustomer(Customers customers) throws SQLException;
